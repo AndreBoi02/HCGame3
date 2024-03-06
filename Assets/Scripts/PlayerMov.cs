71db4e07fa67482dffe0aa4facd9e56af7483c55
AndreBoi02 <97614174+AndreBoi02@users.noreply.github.com>
2024-03-06 08:52:43 -0600

    Move Commit

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -5,15 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMov : MonoBehaviour
 {
-    //El código contiene referencias del script que el profe nos enseñó para saber cómo usar el New InputSystem
-
-    //Declaramos una
     private PlayerInputActions playerInputActions;
-
     private Rigidbody2D rb;
     private BoxCollider2D bc;
 
     [SerializeField] private float jumpForce;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isCrouching;
 
     private void Awake()
     {
@@ -31,12 +29,29 @@ public class PlayerMov : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (isGrounded && !isCrouching) // Solo permite saltar si el jugador está en el suelo y no está agachado
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
 
     private void Crouch(InputAction.CallbackContext context)
     {
-        
+        if (context.ReadValue<float>() > 0) // El jugador está agachándose
+        {
+            GetComponent<SpriteRenderer>().size = new Vector2(GetComponent<SpriteRenderer>().size.x, 1);
+            GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<BoxCollider2D>().size.x, 1);
+            isCrouching = true;
+        }
+        else // El jugador deja de agacharse
+        {
+            GetComponent<SpriteRenderer>().size = new Vector2(GetComponent<SpriteRenderer>().size.x, 2);
+            GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<BoxCollider2D>().size.x, 2);
+            isCrouching = false;
+        }
     }
 
+    // Asegúrate de actualizar la variable isGrounded en algún lugar de tu código
+    // Por ejemplo, podrías hacerlo en OnCollisionEnter2D y OnCollisionExit2D
 }
